46437af85671cb90ab9f9ac71fa1977d1bb6016f
kwon-wooyoung <rnjsdndud00@naver.com>
2025-07-01 20:09:54 +0900

    refactor: 예외 응답 코드 관리 객체 생성

--- src/main/java/com/planit/calendar/exception/ExceptionResponseDto.java
+++ src/main/java/com/planit/calendar/exception/ExceptionResponseDto.java
@@ -1,11 +1,23 @@
 package com.planit.calendar.exception;
 
+import com.planit.calendar.response.ResponseCode;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 @AllArgsConstructor
 public class ExceptionResponseDto {
-    private HttpStatus statusCode;
+    private HttpStatus status; // HTTP 상태 코드
+    private String code; // 내부 정의 코드
     private String errorMessage;
-    private String Path;
+    private String path; // 요청된 경로
+
+    public ExceptionResponseDto(ResponseCode responseCode, HttpServletRequest request) {
+        this.status = HttpStatus.valueOf(responseCode.getStatus());
+        this.code = responseCode.getCode();
+        this.errorMessage = responseCode.getMessage();
+        this.path = request.getRequestURI();
+    }
 }
--- src/main/java/com/planit/calendar/exception/GlobalExceptionHandler.java
+++ src/main/java/com/planit/calendar/exception/GlobalExceptionHandler.java
@@ -2,30 +2,32 @@ package com.planit.calendar.exception;
 
 import com.planit.calendar.exception.custom.DuplicateException;
 import com.planit.calendar.exception.custom.NotFoundException;
+import com.planit.calendar.response.ResponseCode;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 /**
  * 시스템에서 발생하는 모든 예외를 처리하는 글로벌 핸들러
  */
+@Slf4j
 @RestControllerAdvice
 @Hidden
 public class GlobalExceptionHandler {
     /**
-     * Exception 에러가 들어오면 BadRequest(400) 상태코드와 함께 에러 메시지를 반환한다.
+     * Exception 에러가 들어오면 InternalServerError(500) 상태코드와 함께 에러 메시지를 반환한다.
      */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponseDto> handleException(Exception exception, HttpServletRequest request) {
-        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
-            HttpStatus.BAD_REQUEST,
-            exception.getMessage(),
-            request.getRequestURI()
-        );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponseDto);
+        log.error("예상치 못한 예외 발생. 요청 URI: {}, 예외 메시지: {}", request.getRequestURI(), exception.getMessage());
+        ResponseCode responseCode = ResponseCode.INTERNAL_SERVER_ERROR;
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(responseCode, request);
+        return ResponseEntity.status(responseCode.getStatus()).body(exceptionResponseDto);
     }
 
     /**
@@ -33,12 +35,10 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ExceptionResponseDto> handleRuntimeException(RuntimeException exception, HttpServletRequest request) {
-        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            exception.getMessage(),
-            request.getRequestURI()
-        );
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exceptionResponseDto);
+        log.error("예상치 못한 런타임 예외 발생. 요청 URI: {}, 예외 메시지: {}", request.getRequestURI(), exception.getMessage());
+        ResponseCode responseCode = ResponseCode.INTERNAL_SERVER_ERROR;
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(responseCode, request);
+        return ResponseEntity.status(responseCode.getStatus()).body(exceptionResponseDto);
     }
 
     /**
@@ -46,25 +46,21 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponseDto> handleIllegalArgumentException(IllegalArgumentException exception, HttpServletRequest request) {
-        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
-            HttpStatus.BAD_REQUEST,
-            exception.getMessage(),
-            request.getRequestURI()
-        );
-        return ResponseEntity.badRequest().body(exceptionResponseDto);
+        log.warn("잘못된 인자 예외 발생. 요청 URI: {}, 예외 메시지: {}", request.getRequestURI(), exception.getMessage());
+        ResponseCode responseCode = ResponseCode.BAD_REQUEST;
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(responseCode, request);
+        return ResponseEntity.status(responseCode.getStatus()).body(exceptionResponseDto);
     }
 
     /**
-     * 시스템 상태가 요청을 수행할 수 없는 경우 Forbidden(403) 상태코드와 함께 에러 메시지를 반환한다.
+     * 시스템 상태가 요청을 수행할 수 없는 경우 BadRequest(400) 상태코드와 함께 에러 메시지를 반환한다.
      */
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ExceptionResponseDto> handleIllegalStateException(IllegalStateException exception, HttpServletRequest request) {
-        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
-            HttpStatus.FORBIDDEN,
-            exception.getMessage(),
-            request.getRequestURI()
-        );
-        return ResponseEntity.badRequest().body(exceptionResponseDto);
+        log.warn("서버의 상태에 어긋나는 파라미터로 요청. 요청 URI: {}, 예외 메시지: {}", request.getRequestURI(), exception.getMessage());
+        ResponseCode responseCode = ResponseCode.BAD_REQUEST;
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(responseCode, request);
+        return ResponseEntity.status(responseCode.getStatus()).body(exceptionResponseDto);
     }
 
     /**
@@ -72,12 +68,10 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponseDto> handleNotFoundException(NotFoundException exception, HttpServletRequest request) {
-        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
-            HttpStatus.NOT_FOUND,
-            exception.getMessage(),
-            request.getRequestURI()
-        );
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponseDto);
+        log.warn("존재하지 않는 리소스 요청. 요청 URI: {}, 예외 메시지: {}", request.getRequestURI(), exception.getMessage());
+        ResponseCode responseCode = ResponseCode.NOT_FOUND;
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(responseCode, request);
+        return ResponseEntity.status(responseCode.getStatus()).body(exceptionResponseDto);
     }
 
     /**
@@ -85,12 +79,22 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(DuplicateException.class)
     public ResponseEntity<ExceptionResponseDto> handleDuplicateException(DuplicateException exception, HttpServletRequest request) {
-        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
-            HttpStatus.CONFLICT,
-            exception.getMessage(),
-            request.getRequestURI()
-        );
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionResponseDto);
+        log.warn("중복되는 데이터로 요청. 요청 URI: {}, 예외 메시지: {}", request.getRequestURI(), exception.getMessage());
+        ResponseCode responseCode = ResponseCode.CONFLICT;
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(responseCode, request);
+        return ResponseEntity.status(responseCode.getStatus()).body(exceptionResponseDto);
+    }
+
+    /**
+     * 요청 파라미터 값이 형식과 다를 때 BadRequest(400) 상태코드와 함께 에러 메시지를 반환한다.
+     * 예를 들어 2025-12-32 같은 값이 들어오는 경우
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionResponseDto> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception, HttpServletRequest request) {
+        log.warn("요청 파라미터 형식 오류. 요청 URI: {}, 예외 메시지: {}", request.getRequestURI(), exception.getMessage());
+        ResponseCode responseCode = ResponseCode.INVALID_PARAMETER;
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(responseCode, request);
+        return ResponseEntity.badRequest().body(exceptionResponseDto);
     }
 
 }
